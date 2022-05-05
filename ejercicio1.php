567d3feb0a6b424226ecae154ed0b768f8c32f7e
frankligomez <f.gomez@misena.edu.co>
2022-05-05 17:37:39 -0500

    20220505_Frankli

--- ejercicio1.php
+++ ejercicio1.php
@@ -1,8 +1,18 @@
 <?php
 
-//Instrucción de impresión de información
+//Instrucción de impresión de información solo sirve para una sola línea
 
-        echo "Hola mundo";
+echo "Hola Frankli desde el echo";
 
+/* 
+Instrucción de impresión en pantalla
+Nos permite agragar comentarios largos
+En varias líneas
+
+*/
+
+print_r("Hola Frankli desde el Print_R, la cual imprime más información");
+
+print("Hola Frankli desde el Print");
 
 ?>
